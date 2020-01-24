6a5bf6b2c3bf55c272810d499966c821c1e18070
kazawa.ichirou@gmail.com <kazawa.ichirou@gmail.com>
2020-01-24 16:52:42 +0900

    zip修正

--- pkg/zip/dic.go
+++ pkg/zip/dic.go
@@ -3,7 +3,22 @@ package zip
 import (
 	azip "archive/zip"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"strings"
+
+	"golang.org/x/text/encoding/japanese"
 )
+
+// ZippedFile はzipのエントリとしてのファイルです。
+type ZippedFile interface {
+	NameUTF8() string
+	File() *azip.File
+}
+type file struct {
+	file *azip.File
+	dic  *dic
+}
 
 // Dictionary は zip 内のファイルの辞書です。
 type Dictionary interface {
@@ -11,61 +26,115 @@ type Dictionary interface {
 	Contains(filepath string) bool
 	// Get は、そのファイルの中身を取得します。
 	Get(filepath string) ([]byte, error)
+	// GetReader は、そのファイルのReaderを取得します。
+	GetReader(filepath string) (io.ReadCloser, error)
+	// FileHeader は、そのファイルの情報を取得します。
+	File(filepath string) ZippedFile
 	// FilePaths はファイルの一覧を取得します。
 	FilePaths() []string
 	// Close は Dictionary をクローズします。
 	Close()
+	// 大文字小文字を区別する場合は真を返します。
+	IsIgnoreCapital() bool
 }
 
 type dic struct {
-	arc   *azip.ReadCloser
-	dic   map[string]*azip.File
-	files []string
+	arc           *azip.ReadCloser
+	dic           map[string]ZippedFile
+	files         []string
+	ignoreCapital bool
 }
 
 // OpenDictionary は Dictionary を取得します。
-func OpenDictionary(zippath string) (Dictionary, error) {
+func OpenDictionary(zippath string, ignoreCapital bool) (Dictionary, error) {
 	r, err := azip.OpenReader(zippath)
 	if err != nil {
 		return nil, err
 	}
-	dic := &dic{arc: r, dic: map[string]*azip.File{}, files: []string{}}
+	dic := &dic{arc: r, dic: map[string]ZippedFile{}, files: []string{}, ignoreCapital: ignoreCapital}
 
 	for _, f := range dic.arc.File {
-		if f.FileInfo().IsDir() {
-			continue
-		}
-		dic.dic[f.Name] = f
-		dic.files = append(dic.files, f.Name)
+		// if f.FileInfo().IsDir() {
+		// 	continue
+		// }
+		file := &file{file: f, dic: dic}
+		name := file.NameUTF8()
+		dic.dic[name] = file
+		dic.files = append(dic.files, name)
 	}
 	return dic, nil
 }
 
+// Contains は、そのファイルが存在する場合に true を返します。
 func (d *dic) Contains(filepath string) bool {
+	if d.ignoreCapital {
+		filepath = strings.ToLower(filepath)
+	}
 	_, ok := d.dic[filepath]
 	return ok
 }
+
+// Get は、そのファイルの中身を取得します。
 func (d *dic) Get(filepath string) ([]byte, error) {
-	f, ok := d.dic[filepath]
-	if !ok {
-		return []byte{}, fmt.Errorf("filepath not found: %s", filepath)
-	}
-	rc, err := f.Open()
+	rc, err := d.GetReader(filepath)
 	if err != nil {
-		return []byte{}, err
+		return nil, err
 	}
 	defer rc.Close()
-	if f.UncompressedSize64 == 0 {
-		// サイズ0のファイルをRead()するとEOFエラーとなるため
-		return []byte{}, nil
-	}
-	arr := make([]byte, f.UncompressedSize64)
-	_, rerr := rc.Read(arr)
-	return arr, rerr
+	return ioutil.ReadAll(rc)
 }
+
+// GetReader は、そのファイルの中身を取得します。
+func (d *dic) GetReader(filepath string) (io.ReadCloser, error) {
+	f := d.File(filepath)
+	if f == nil {
+		return nil, fmt.Errorf("filepath not found: %s", filepath)
+	}
+	return f.File().Open()
+}
+
+// FileHeader は、そのファイルの情報を取得します。
+func (d *dic) File(filepath string) ZippedFile {
+	if d.ignoreCapital {
+		filepath = strings.ToLower(filepath)
+	}
+	f, ok := d.dic[filepath]
+	if !ok {
+		return nil
+	}
+	return f
+}
+
+// FilePaths はファイルの一覧を取得します。
 func (d *dic) FilePaths() []string {
 	return d.files
 }
+
+// Close は Dictionary をクローズします。
 func (d *dic) Close() {
 	d.arc.Close()
+}
+
+// 大文字小文字を区別する場合は真を返します。
+func (d *dic) IsIgnoreCapital() bool {
+	return d.ignoreCapital
+}
+
+// NameUTF8 はUTF8に正規化したファイル名を取得します。
+func (f *file) NameUTF8() string {
+	name := f.file.Name
+	if f.file.NonUTF8 {
+		if n, err := japanese.ShiftJIS.NewDecoder().String(name); err == nil {
+			name = n
+		}
+	}
+	if f.dic.ignoreCapital {
+		// キーは小文字で統一
+		name = strings.ToLower(name)
+	}
+	return name
+}
+
+func (f *file) File() *azip.File {
+	return f.file
 }
--- pkg/zip/dic_test.go
+++ pkg/zip/dic_test.go
@@ -8,15 +8,19 @@ import (
 
 func TestDic(t *testing.T) {
 	// OpenDictionary
-	dic, e := OpenDictionary(testutil.GetFilepath("../../test/notexists.zip"))
+	dic, e := OpenDictionary(testutil.GetFilepath("../../test/notexists.zip"), true)
 	if e == nil {
 		t.Fail()
 	}
-	dic, e = OpenDictionary(testutil.GetFilepath("../../test/test.zip"))
+	dic, e = OpenDictionary(testutil.GetFilepath("../../test/test.zip"), false)
 	if e != nil {
 		t.Fatal(e)
 	}
 	defer dic.Close()
+
+	if dic.IsIgnoreCapital() {
+		t.Fatal()
+	}
 
 	// FilePaths
 	for _, filepath := range dic.FilePaths() {
@@ -27,7 +31,10 @@ func TestDic(t *testing.T) {
 	if dic.Contains("notexists.txt") {
 		t.Fail()
 	}
-	if !dic.Contains("test1.txt") {
+	if dic.Contains("test1.txt") {
+		t.Fail()
+	}
+	if false == dic.Contains("Test1.txt") {
 		t.Fail()
 	}
 
@@ -36,7 +43,70 @@ func TestDic(t *testing.T) {
 	if e == nil {
 		t.Fail()
 	}
-	b, e := dic.Get("test1.txt")
+	_, e = dic.Get("test1.txt")
+	if e == nil {
+		t.Fatal(e)
+	}
+	b, e := dic.Get("Test1.txt")
+	if e != nil {
+		t.Fatal(e)
+	}
+	s := string(b)
+	if s != "test1.txt\r\n" {
+		t.Fatal(s, b)
+	}
+	b, e = dic.Get("empty.txt")
+	if e != nil {
+		t.Fatal(e)
+	}
+	s = string(b)
+	if s != "" {
+		t.Fatal(s, b)
+	}
+}
+
+func TestDicIgnoreCapital(t *testing.T) {
+	// OpenDictionary
+	dic, e := OpenDictionary(testutil.GetFilepath("../../test/notexists.zip"), true)
+	if e == nil {
+		t.Fail()
+	}
+	dic, e = OpenDictionary(testutil.GetFilepath("../../test/test.zip"), true)
+	if e != nil {
+		t.Fatal(e)
+	}
+	defer dic.Close()
+
+	if false == dic.IsIgnoreCapital() {
+		t.Fatal()
+	}
+
+	// FilePaths
+	for _, filepath := range dic.FilePaths() {
+		t.Logf("%+v\n", filepath)
+	}
+
+	// Contains
+	if dic.Contains("notexists.txt") {
+		t.Fail()
+	}
+	if false == dic.Contains("test1.txt") {
+		t.Fail()
+	}
+	if false == dic.Contains("Test1.txt") {
+		t.Fail()
+	}
+
+	// Get
+	_, e = dic.Get("notexists.txt")
+	if e == nil {
+		t.Fail()
+	}
+	_, e = dic.Get("test1.txt")
+	if e != nil {
+		t.Fatal(e)
+	}
+	b, e := dic.Get("Test1.txt")
 	if e != nil {
 		t.Fatal(e)
 	}
